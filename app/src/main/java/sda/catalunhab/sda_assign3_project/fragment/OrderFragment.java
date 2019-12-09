7007257e751c73b60472338ffa80acad66809856
catalunhab <bianca.catalunha@paddypowerbetfair.com>
2019-12-09 01:46:35 +0000

    Created a radio group with the two options(delivery/collection) and set a listener

--- app/src/main/java/sda/catalunhab/sda_assign3_project/fragment/OrderFragment.java
+++ app/src/main/java/sda/catalunhab/sda_assign3_project/fragment/OrderFragment.java
@@ -2,12 +2,15 @@ package sda.catalunhab.sda_assign3_project.fragment;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import sda.catalunhab.sda_assign3_project.R;
 
@@ -16,6 +19,8 @@ import sda.catalunhab.sda_assign3_project.R;
  */
 public class OrderFragment extends Fragment {
 
+    private Spinner daysDropDown;
+    private TextView deliveryAddress;
 
     public OrderFragment() {
         // Required empty public constructor
@@ -25,8 +30,44 @@ public class OrderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false);
+        View root = inflater.inflate(R.layout.fragment_order, container, false);
+
+        deliveryAddress = root.findViewById(R.id.deliveryAddress);
+
+        RadioGroup radioGroup = root.findViewById(R.id.radioGroup);
+        setOnCheckedChangeListener(radioGroup);
+
+        setDaysDropdownValues(root);
+
+        return root;
     }
 
+    private void setDaysDropdownValues(View root) {
+        daysDropDown = root.findViewById(R.id.daysDropDown);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(root.getContext(),
+                R.array.collection_period, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        daysDropDown.setAdapter(adapter);
+    }
+
+    private void setOnCheckedChangeListener(RadioGroup radioGroup) {
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radioButton1:
+                            deliveryAddress.setVisibility(View.VISIBLE);
+                            daysDropDown.setVisibility(View.INVISIBLE);//set to invisible so they won't be on top of each other
+                        break;
+                    case R.id.radioButton2:
+                            daysDropDown.setVisibility(View.VISIBLE);
+                            deliveryAddress.setVisibility(View.INVISIBLE);
+                        break;
+                }
+            }
+        });
+    }
 }
