9b17863c668452fb0d872bfbb639bd8d7b881fa9
catalunhab <bianca.catalunha@paddypowerbetfair.com>
2019-12-12 18:31:14 +0000

    Added products, image resize

--- app/src/main/java/sda/catalunhab/sda_assign3_project/fragment/ProductsFragment.java
+++ app/src/main/java/sda/catalunhab/sda_assign3_project/fragment/ProductsFragment.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -22,12 +22,12 @@ public class ProductsFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-       View root = inflater.inflate(R.layout.fragment_item_list, container, false);
+        View root = inflater.inflate(R.layout.fragment_item_list, container, false);
 
         RecyclerView recyclerView = root.findViewById(R.id.recyclerView);
 
         // use a linear layout manager
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(root.getContext());
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(root.getContext(), 2);
         recyclerView.setLayoutManager(layoutManager);
 
         // specify an adapter (see also next example)
@@ -41,8 +41,12 @@ public class ProductsFragment extends Fragment {
         ArrayList<Product> products = new ArrayList<>();
 
         products.add(new Product("Belly Jacket", "€37.00", R.drawable.belly_jacket));
-        products.add(new Product("Formal Shirt", "€23.00", R.drawable.formal_shirt));
-
+        products.add(new Product("Off Shoulder", "€23.00", R.drawable.off_shoulder));
+        products.add(new Product("Belly Top", "€26.00", R.drawable.belly_top));
+        products.add(new Product("Jacket", "€41.00", R.drawable.jacket));
+        products.add(new Product("Half Sleeve Top", "€19.00", R.drawable.half_sleeve_top));
+        products.add(new Product("Casual Dress", "€42.00", R.drawable.casual_dress));
+        products.add(new Product("Party Dress", "€75.00", R.drawable.party_dress));
 
         return products;
     }
